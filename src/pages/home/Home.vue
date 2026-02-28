3dbe6e0836e16dfd2966f3307ae64de5fb6a1ddd
Ana Ron <aronman13@gmail.com>
2026-02-28 07:40:12 +0100

    Creación de rutas y botones simples a las páginas principales

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import Button from '@/components/ui/button/Button.vue';
+
 
 
 </script>
@@ -7,6 +9,19 @@
 
 <h1>Diseño web y apps interactivas</h1>
 
+<div class="menu">
+      <RouterLink to="/gallery">
+        <Button>Galería</Button>
+      </RouterLink>
+
+      <RouterLink to="/contact">
+        <Button>Contacto</Button>
+      </RouterLink>
+
+      <RouterLink to="/info">
+        <Button>Información</Button>
+      </RouterLink>
+</div>
 
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,10 @@
+
+
+import Contacto from "@/pages/contact/Contacto.vue";
+import Trabajos from "@/pages/gallery/Trabajos.vue";
 import Home from "@/pages/home/Home.vue";
+import Info from "@/pages/iformartion/Info.vue";
+
 import { createRouter, createWebHashHistory } from "vue-router";
 
 
@@ -11,6 +17,21 @@ export const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: Trabajos
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: Info
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contacto
     },
     {
       path: '/:patchMatch(.*)',
